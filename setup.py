8966e08e6b72cdadbd8c385316fef1087924a47d
shivamshukla12 <88490443+shivamshukla12@users.noreply.github.com>
2021-08-06 13:29:13 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+# calling the setup function 
+setup(name='dl2bq',
+      version='0.0.1',
+      description='A Data Migration API for Data Lake to BQ',
+      py_modules=["dl2bq"],
+      author='shivam shukla',
+      author_email='shivamshukla12@gmail.com',
+      package_dir = {' ':r'C:\Users\i339715\Desktop\dl2bq\src'}
+      )
